835c8cc595289c07232799f14e7fe0c99d976030
Sebastián Silva <sgsilvas96@gmail.com>
2022-04-06 08:58:32 +0200

    add .

--- R/main.r
+++ R/main.r
@@ -1,8 +1,10 @@
 # Call libraries
 suppressMessages({
   library(xcms); library(tidyverse); library(data.table); library(MSnbase)
-  library(CAMERA); library(glycanPredict); library(viridis)
+  library(CAMERA); library(glycanPredict); library(viridis); library(gridExtra)
 })
+
+devtools::install_github("margotbligh/glycanPredict")
 
 ppm_to_mz = function(mz, noise){
   ppm = mz / 1000000 * noise
@@ -27,6 +29,8 @@ predictGlycansParams = setClass("predictGlycansParams",
                                                     label="none",
                                                     modifications=c("deoxy", "unsaturated", "sulphate")))
 
+a = CentWaveParam()
+ppm(a)
 
 Dulce_fetch = function(data, cwp=NULL, pdp=NULL, return_everything=F){
   
@@ -42,7 +46,7 @@ Dulce_fetch = function(data, cwp=NULL, pdp=NULL, return_everything=F){
   # Defining a default "Ungrouped" category if no grouping is defined.
   if (length(pdp@sampleGroups)==0){
     pdp@sampleGroups = rep("Ungrouped", nrow(data))
-    message("Dulce warning: No sample groups were defined. One group under the name of 'Ungrouped' will be created.")}
+    message("Dulce warning: No sample groups were defined. .,name of 'Ungrouped' will be created.")}
   
   # Fetch 
   processed_data = findChromPeaks(data, param=cwp) %>% groupChromPeaks(param=pdp)
@@ -62,7 +66,7 @@ Dulce_fetch = function(data, cwp=NULL, pdp=NULL, return_everything=F){
 Dulce_toCAMERA = function(data, names=NULL, classes="Unclassified"){
   
   if (class(data)!="XCMSnExp"){stop("Dulce error: 'data' object is not from 'XCMSnExp' class.")}
-  suppressMessages(data_converted = as(data, "xcmsSet"))
+  suppressMessages({data_converted = as(data, "xcmsSet")})
   
   if (is.null(names)){
     names = sprintf("sample_%03d", 1:nrow(data))
@@ -130,10 +134,10 @@ Dulce_annotate = function(data, pgp=NULL,
     stop("Dulce error: 'cwp' is not an 'predictGlycansParams' object. Check for ?predictGlycansParams or set it as NULL to use default arguments.")}
   
   
-  predicted = predictGlycans(dp1=dp1, dp2=dp2, ESI_mode=ESI_mode, 
-                             scan_range1=scan_range1, scan_range2=scan_range2,
-                             pent_option=pent_option, modifications=modifications, 
-                             label = label) %>% 
+  predicted = predictGlycans(dp1=pgp@dp1, dp2=pgp@dp2, ESI_mode=pgp@ESI_mode, 
+                             scan_range1=pgp@scan_range1, scan_range2=pgp@scan_range2,
+                             pent_option=pgp@pent_option, modifications=pgp@modifications, 
+                             label=pgp@label) %>% 
     pivot_longer(-c(name, dp, mass, formula), names_to = "ion", values_to = "mz") %>% 
     drop_na()
   
@@ -160,9 +164,7 @@ Dulce_doAll = function(data, cwp=NULL, pdp=NULL,
                        isotopes=T, adducts=T, perfwhm=0.5, mzabs_isotAdd=0.01, 
                        cor_eic_th=0.75, polarity=NULL,
                        rtmin=0, rtmax=Inf,
-                       dp1=1, dp2=6, ESI_mode="neg", scan_range1=0, scan_range2=2000, 
-                       pent_option=1, label="none",
-                       modifications=c("deoxy", "unsaturated", "sulphate"),
+                       pgp=NULL,
                        ppm=NULL, mzabs=NULL,
                        return_everything=F){
   
@@ -178,10 +180,7 @@ Dulce_doAll = function(data, cwp=NULL, pdp=NULL,
   data_annotated = Dulce_find(data_converted, isotopes=isotopes, adducts=adducts, 
                                   perfwhm=perfwhm, mzabs=mzabs_isotAdd, cor_eic_th=cor_eic_th, polarity=polarity)
   data_trimmed = Dulce_trimIsotopes(data_annotated, rtmin=rtmin, rtmax=rtmax)
-  data_predicted = Dulce_annotate(data_trimmed, dp1=dp1, dp2=dp2, ESI_mode=ESI_mode, 
-                                 scan_range1=scan_range1, scan_range2=scan_range2, 
-                                 pent_option=pent_option, label=label,
-                                 modifications=modifications,
+  data_predicted = Dulce_annotate(data_trimmed, pgp=pgp,
                                  ppm=ppm, mzabs=mzabs)
   
   if (return_everything){
@@ -197,7 +196,77 @@ Dulce_doAll = function(data, cwp=NULL, pdp=NULL,
   return(data_predicted)
 }
 
-
+Dulce_display = function(data, peak=NULL, rtime_window=10, ppm_window=25, rt_scale=1, mz_scale=10){
+  
+  # Checks
+  
+  # Peak limits
+  mz_min = peak$mzmin
+  mz_max = peak$mzmax
+  rt_min = peak$rtmin
+  rt_max = peak$rtmax
+  
+  # Plot limits
+  mz_window = ppm_to_mz(peak$mz, ppm_window) * mz_scale
+  mz_bottom = mz_min - mz_window 
+  mz_top = mz_max + mz_window   
+  rt_bottom = rt_min - rtime_window * rt_scale
+  rt_top = rt_max + rtime_window * rt_scale
+  
+  # Gathering data
+  data_header = header(data)
+  spectra_window = data_header %>% filter(between(retentionTime, rt_bottom, rt_top))
+  
+  data = data[data@featureData@data$spIdx %in% spectra_window$spectrum]
+  
+  neighbourhood = data.frame(mz = unlist(mz(data), use.names=F),
+                             intensity = unlist(intensity(data), use.names=F),
+                             rt = rep(rtime(data), lapply(mz(data), length))) %>% 
+                  filter(between(mz, mz_bottom, mz_top))
+  
+  rt_mz = ggplot(neighbourhood, aes(x=rt, y=mz, col=intensity)) +
+    geom_point(size=2, alpha=0.8) +
+    scale_color_viridis(option="magma") +
+    ggplot2::annotate(geom="rect",
+                      ymin=mz_min,
+                      ymax=mz_max,
+                      xmin=rt_min, 
+                      xmax=rt_max,
+                      alpha=0.1, fill="red", col="red") + 
+    theme_bw() + theme(legend.position = "none") + 
+    ylim(c(mz_bottom, mz_top)) + xlim(c(rt_bottom, rt_top)) +  
+    labs(x="Retention time (seconds)",
+         y="M/z")
+  
+  rt_intensity = ggplot(neighbourhood %>% filter(between(mz, mz_min, mz_max)), 
+                        aes(x=rt, y=intensity)) + 
+    geom_line() + 
+    ggplot2::annotate(geom="rect", 
+                      xmin=rt_min, 
+                      xmax=rt_max,
+                      ymin=-Inf, 
+                      ymax=Inf, 
+                      alpha=0.1, fill="red", col="red")+
+    xlim(c(rt_bottom, rt_top)) + 
+    theme_bw() + labs(x=NULL) + theme(legend.position="none")
+  
+  mz_intensity = ggplot(neighbourhood, aes(x=round(mz,3), y=intensity)) + 
+    geom_col() + 
+    ggplot2::annotate(geom="rect", 
+                      xmin=mz_min,
+                      xmax=mz_max,
+                      ymin=-Inf, 
+                      ymax=Inf, 
+                      alpha=0.1, fill="red", col="red")+
+    theme_bw() + coord_flip() + labs(x=NULL) + 
+    xlim(c(mz_bottom, mz_top))
+  
+  empty = ggplot() + theme_void() + ggplot2::annotate(geom="text", x=0, y=0, label=paste0("peak ",peak$sn))
+  
+  plot = arrangeGrob(rt_intensity, empty, rt_mz, mz_intensity, 
+                     ncol=2, nrow=2, widths=c(4, 1), heights=c(1, 4))
+  return(plot)
+}
 
 # Sample code -------------------------------------------------------------
 file.paths = dir(path="mzML_example3", all.files=F, full.names=T)
@@ -232,14 +301,15 @@ data = readMSData(files=file.paths,
 # Filter by only MS data (not MS/MS)
 data = data[data@featureData@data$msLevel==1]
 
+data = data %>% Dulce_fetch(return_everything = T)
+peak = data$peaks[1,]
+
+ggsave(filename=paste0("C:/Users/ssilva/ownCloud/Lab rotations/R with Margot/Playground/R/", peak$sn, ".png"),
+       Dulce_display(data$data, peak),
+       width=4, height=3, unit)
+
+mz(data$data)
 register(SerialParam())
-
-pgp = predictGlycansParams(dp1=1, dp2=6, ESI_mode="neg", scan_range1=0, scan_range2=2000, 
-                          pent_option=1, label="none",
-                          modifications=c("deoxy", "unsaturated", "sulphate"))
-    
-
-
 
 data_processed = Dulce_doAll(data, polarity="negative", ppm=10)
 
